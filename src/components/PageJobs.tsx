e764391dd30c7bb520010ec69cf0c019a30b2f32
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-17 05:38:44 +0100

    simple job data showing

--- src/components/PageJobs.tsx
+++ src/components/PageJobs.tsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:5556';
+
+interface IJob {
+	id: number,
+	title: string;
+	company: string;
+	url: string;
+	skills: string;
+	publicationDate: string;
+	status: string;
+}
+
 export const PageJobs = () => {
+	const [jobs, setJobs] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setJobs((await axios.get(`${apiUrl}/jobs`)).data);
+		})();
+	}, []);
+
 	return (
-		<>
-			<p>This is the Jobs page.</p>
-		</>
+		<div className="pageJobs">
+			<p>There are {jobs.length} jobs:</p>
+			<div className="jobs">
+				<h3>Jobs</h3>
+				<ul>
+					{jobs.map((job: IJob) => {
+						return <li>{job.title}</li>;
+					})}
+				</ul>
+			</div>
+		</div>
 	);
 };
