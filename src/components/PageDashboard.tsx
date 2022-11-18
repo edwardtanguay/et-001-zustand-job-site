8b3bec0bc174ec6302c680348cab1d6d1929ab80
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-18 04:08:24 +0100

    styles added

--- src/components/PageDashboard.tsx
+++ src/components/PageDashboard.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import { AccessAlarm, PhotoCamera, Send } from '@material-ui/icons';
 import TextField from '@mui/material/TextField';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export const PageDashboard = () => {
 	const [message, setMessage] = useState('Please click something, etc.');
@@ -9,12 +11,23 @@ export const PageDashboard = () => {
 		title: 'ttt',
 		company: 'ccc',
 		url: 'uuu',
-		description: 'ddd'
-	})
+		description: 'ddd',
+		publicationDate: '2022-11-17',
+	});
 
 	const handleButtonClick = (text: string) => {
 		setMessage(text);
 	};
+
+	const handleChangeFormField = (obj: any, fieldName: string) => {
+		if (fieldName === 'postingDate') {
+			const chosenDate = obj;
+			const chosenDateIso = chosenDate.toISOString().split('T')[0];
+			formData.publicationDate = chosenDateIso;
+		}
+		setFormData({ ...formData });
+	};
+
 	return (
 		<div className="page pageDashboard">
 			<p className="message">
@@ -115,6 +128,24 @@ export const PageDashboard = () => {
 					margin="normal"
 					placeholder="Enter the name of the company"
 					value={formData.company}
+				/>
+				<TextField
+					id="outlined-multiline-static"
+					fullWidth
+					label="Description"
+					margin="normal"
+					multiline
+					rows={4}
+					placeholder="Enter Description of job as JSON text block"
+					value={formData.company}
+				/>
+				<DatePicker
+					value={formData.publicationDate}
+					selected={new Date()}
+					onChange={(date: Date) =>
+						handleChangeFormField(date, 'postingDate')
+					}
+					className="datePicker"
 				/>
 			</form>
 		</div>
